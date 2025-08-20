cd7f7695f678cdd14e1af51de436930203a7f624
ottsupi <ott.regisorate@gmail.com>
2025-08-20 08:45:37 +0000

    feat: home page hello world greet authenticated users instead

--- src/apps/app/views.py
+++ src/apps/app/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import render
 logger = logging.getLogger(__name__)
 
 
-def index(request):
+def index(request: HttpResponse):
     logger.debug("Accessed Hello world! view")
-
     message = "Hello world!"
+    if request.user.is_authenticated:
+        message = f"Hello {request.user.get_short_name()}!"
+
     context = {
         "message": message,
     }
