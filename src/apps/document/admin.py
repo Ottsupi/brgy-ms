394e1b3320c07ff4f88edfa2990c816bacb26eb2
ottsupi <ott.regisorate@gmail.com>
2025-08-22 11:07:05 +0000

    feat(document): add models to admin

--- src/apps/document/admin.py
+++ src/apps/document/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DocumentCategory, DocumentFile, DocumentType
+
+
+class DocumentCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class DocumentTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class DocumentFileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(DocumentCategory, DocumentCategoryAdmin)
+admin.site.register(DocumentType, DocumentTypeAdmin)
+admin.site.register(DocumentFile, DocumentFileAdmin)
