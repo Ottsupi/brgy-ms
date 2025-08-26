a23464b2fc8415c9fd3851ede7cd69d2053189ef
ottsupi <ott.regisorate@gmail.com>
2025-08-26 06:22:13 +0000

    fix: missing forward slash on document.urls

--- src/apps/document/urls.py
+++ src/apps/document/urls.py
@@ -6,7 +6,7 @@ app_name = "document"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create_view, name="create"),
-    path("request", views.request_list_view, name="request_list"),
-    path("issuable", views.issuable_document_list_view, name="issuable_document_list"),
+    path("create/", views.create_view, name="create"),
+    path("request/", views.request_list_view, name="request_list"),
+    path("issuable/", views.issuable_document_list_view, name="issuable_document_list"),
 ]
